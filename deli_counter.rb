388c1d64331903702928f3075ac61fc3dfc67489
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:09:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,21 @@
 # Write your code here.
 def line(array)
   line = []
-  count = 1
+  position = 1
   if array.size == 0
     return "The line is currently empty."
   else
     for i in array do
-      line.push(" #{count}. #{i}")
+      line.push(" #{position}. #{i}")
       count += 1
     end
     return "The line is currently:" + line
 end
 
-def take_a_number(array)
+def take_a_number(array, string)
+  position = 1
+  array.push(string)
+  puts "Welcome, #{string}. You are number #{position} in line."
 end
 
 def now_serving(array)
