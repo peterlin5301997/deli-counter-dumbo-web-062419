1870c5cb997af221cdc55b5d4ee9a73de25ea074
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:31:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(array)
+  line = []
+  position = 1
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    for i in array do
+      message += " #{position}. #{i}"
+      position += 1
+    end
+    puts message
+  end
+end
+
+def take_a_number(array, string)
+  array.push(string)
+  puts "Welcome, #{string}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
