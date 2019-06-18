93d8ac133dbc4d960d87620c518169faa9b602cf
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:14:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
       line.push(" #{position}. #{i}")
       position += 1
     end
-    puts "The line is currently:" + line
+    puts "The line is currently:" + line.to_s
   end
 end
 
