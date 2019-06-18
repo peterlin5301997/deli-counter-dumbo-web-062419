8d39c8a280a7101ccb2e19c9f0213a6ed0762983
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 00:07:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,15 @@
 # Write your code here.
 def line(array)
-
+  line = []
+  count = 1
+  if array.size == 0
+    return "The line is currently empty."
+  else
+    for i in array do
+      line.push(" #{count}. #{i}")
+      count += 1
+    end
+    return "The line is currently:" + line
 end
 
 def take_a_number(array)
